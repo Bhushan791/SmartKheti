f9b477b3e49203a3040114b3a3e86de51548e9fa
Bhushan_bhatta <bhushanbhatta777@gmail.com>
2025-08-02 22:39:09 +0545

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
-    port: 3001,  // your desired port here
+    port: 3001, // your desired port here
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  // This is crucial for SPA routing
+  preview: {
+    port: 3001,
+  }
 })
