470cbd8e4ef8df18b9ddce45b538f9de94c1c3a8
Bhushan_bhatta <bhushanbhatta777@gmail.com>
2025-08-03 02:00:18 +0545

    nice mistake

--- server/smartkheti/users/models.py
+++ server/smartkheti/users/models.py
@@ -16,23 +16,23 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-def create_superuser(self, phone, password=None, **extra_fields):
-    extra_fields.setdefault('is_staff', True)
-    extra_fields.setdefault('is_superuser', True)
-    extra_fields.setdefault('first_name', 'Admin')
-    extra_fields.setdefault('last_name', '')
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('first_name', 'Admin')
+        extra_fields.setdefault('last_name', '')
 
-    if extra_fields.get('is_staff') is not True:
-        raise ValueError('Superuser must have is_staff=True.')
-    if extra_fields.get('is_superuser') is not True:
-        raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
-    user = self.model(phone=phone, **extra_fields)
-    user.set_password(password)
-    user.is_staff = True
-    user.is_superuser = True
-    user.save(using=self._db)
-    return user
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
